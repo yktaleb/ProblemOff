364e28381e6630bfb5e628051f747402f3c08069
yktaleb <taleb.yarik@gmail.com>
2017-10-31 01:20:18 +0200

    Created project structure

---
+++ src/main/java/com/hw/ProblemOffApplication.java
@@ -0,0 +1,12 @@
+package com.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemOffApplication.class, args);
+	}
+}
