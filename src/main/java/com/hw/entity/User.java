bcf8821ab19be59fa19494a09e68d448bedcb61f
yktaleb <taleb.yarik@gmail.com>
2017-10-31 23:19:15 +0200

    Created User entity

--- src/main/java/com/hw/entity/User.java
+++ src/main/java/com/hw/entity/User.java
@@ -2,9 +2,11 @@ package com.hw.entity;
 
 import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Data
 @Getter
@@ -16,6 +18,18 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(nullable = false)
+    private String phoneNumber;
+    private UserSex sex;
+    private Date dateBirthday;
+    private String description;
+    private Address address;
 }
