440a87e6892794c5785385fa0760ecab4fde0ebd
yktaleb <taleb.yarik@gmail.com>
2017-11-02 02:39:35 +0200

    Added cors bean in order to work with client

---
+++ src/main/java/com/hw/bean/CorsBean.java
@@ -0,0 +1,24 @@
+package com.hw.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.persistence.Column;
+
+@Component
+public class CorsBean {
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("https://problem-off.firebaseapp.com");
+            }
+        };
+    }
+}
