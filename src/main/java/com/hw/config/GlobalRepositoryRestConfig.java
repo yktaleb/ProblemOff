b52751fe7b76e35cc1fbe709c6b37f5fcddc8b3a
yktaleb <taleb.yarik@gmail.com>
2017-11-07 21:40:28 +0200

    Back to cors config

--- src/main/java/com/hw/config/GlobalRepositoryRestConfig.java
+++ src/main/java/com/hw/config/GlobalRepositoryRestConfig.java
@@ -7,14 +7,17 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapt
 @Configuration
 public class GlobalRepositoryRestConfig extends RepositoryRestConfigurerAdapter {
 
-//    @Override
-//    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-//        config.getCorsRegistry()
-//                .addMapping("/**")
-//                .allowedOrigins("*")
-////                .allowedHeaders("*")
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getCorsRegistry()
+                .addMapping("/**")
+                .allowedOrigins("*")
+//                .allowedHeaders("*")
 //                .exposedHeaders("X-Auth-Token")
 //                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-//    }
+                .allowedHeaders("X-Auth-Token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+                .exposedHeaders("X-Auth-Token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+                .allowCredentials(false).maxAge(3600);
+    }
 
 }
--- src/main/java/com/hw/util/bean/Beans.java
+++ src/main/java/com/hw/util/bean/Beans.java
@@ -1,42 +1,37 @@
 package com.hw.util.bean;
 
-import com.hw.util.security.CORSFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Component
 public class Beans {
 
-//    @Bean
-//    public WebMvcConfigurer corsConfigure() {
-//        return new WebMvcConfigurerAdapter() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry
-//                        .addMapping("/**")
-//                        .allowedOrigins("*")
-////                        .allowedHeaders("*")
+    @Bean
+    public WebMvcConfigurer corsConfigure() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+//                        .allowedHeaders("*")
 //                        .exposedHeaders("X-Auth-Token")
 //                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-//            }
-//        };
-//    }
+                        .allowedHeaders("X-Auth-Token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+                        .exposedHeaders("X-Auth-Token","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Credentials")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public FilterRegistrationBean someFilterRegistration() {
-
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new CORSFilter());
-        registration.addUrlPatterns("/*");
-        registration.setName("corsFilter");
-        return registration;
-    }
 }
