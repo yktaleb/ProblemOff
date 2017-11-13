7bbf8841a454c4283fd9fabb8b7947c0344ca4f7
yktaleb <taleb.yarik@gmail.com>
2017-11-13 21:16:24 +0200

    Changed cors

--- src/main/java/com/hw/util/security/CORSFilter.java
+++ src/main/java/com/hw/util/security/CORSFilter.java
@@ -23,7 +23,7 @@ public class CORSFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, X-Auth-Token");
