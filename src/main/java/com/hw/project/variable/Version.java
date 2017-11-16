e4ed2077e619bab39bfeae5385c6b129fec41e15
yktaleb <taleb.yarik@gmail.com>
2017-11-16 22:23:30 +0200

    Added plugin which create version variable

---
+++ src/main/java/com/hw/project/variable/Version.java
@@ -0,0 +1,24 @@
+package com.hw.project.variable;
+
+/**
+ * Auto-generated file, contains project and version control state info.
+ */
+public final class Version {
+  public static String PLUGIN_BUILD_TIME = "2017-11-16 22:08";
+
+  public static String PROJECT_NAME = "problem-off";
+  public static String PROJECT_GROUP = "com.hw";
+  public static String PROJECT_ARTIFACT = "problem-off";
+  public static String PROJECT_VERSION = "0.0.1-SNAPSHOT";
+  public static String SOURCE_VERSION = "57c2687";
+  public static String SOURCE_BRANCH = "master";
+  public static Boolean SOURCE_DIRTY = true;
+  public static String SOURCE_WORKING_COPY = "D:/ProblemOffCopy/ProblemOff";
+
+  public static String SERVER_BUILD_PLAN = "N/A";
+  public static String SERVER_BUILD_ID = "N/A";
+  public static String SERVER_BUILD_TIME = "N/A";
+
+  public static String BANNER_FULL = "Build info: problem-off version 0.0.1-SNAPSHOT built on 2017-11-16 22:08 source version 57c2687, build id: N/A-N/A. (dirty-wd) ";
+  public static String BANNER = "0.0.1-SNAPSHOT-57c2687** 2017-11-16 22:08 ";
+}
