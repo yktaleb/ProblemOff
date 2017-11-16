2c628367b2a91f3ba09ec7e7c26a15f42f861159
Kate Bohuslavska <belikeme103@gmail.com>
2017-11-16 22:50:57 +0200

    added not-found test

--- src/test/java/com/hw/controller/api/MainControllerTest.java
+++ src/test/java/com/hw/controller/api/MainControllerTest.java
@@ -68,6 +68,16 @@ public class MainControllerTest {
                 .param("password", "ttt"))
                     .andExpect(status().isOk());
     }
+    
+    @Test
+    public void userNotFoundAfterLogin() throws Exception {
+
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/login")
+                .param("email", "notFoundEmail")
+                .param("password", "ttt"))
+                .andExpect(status().isBadRequest());
+    }
 
     @Test
     public void incorrectPasswordAfterLogin() throws Exception {
